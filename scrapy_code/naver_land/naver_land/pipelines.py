c2cac03fcc70d01a6b787ffd06daaba5413e81f3
leeseungeun <leeseungeun0722@gamil.com>
2022-02-15 17:55:37 +0900

    naver_land

--- scrapy_code/naver_land/naver_land/pipelines.py
+++ scrapy_code/naver_land/naver_land/pipelines.py
@@ -1,45 +1,42 @@
-from msilib import Table
 import mysql.connector
 from itemadapter import ItemAdapter
 
-class NaverLandPipeline(object):
-    def __init__(self):
-        self.create_connection()
-        self.create_table()
+class NaverLandPipeline():
+    # def __init__(self):
+    #     self.create_connection()
+    #     self.create_table()
 
-    def create_connection(self):
-        self.conn = mysql.connector.connect(
-            host = 'localhost',
-            user = 'root',
-            password = '0722',
-            database = 'naver_DB',
-            charset = 'utf8',
-            auth_plugin='mysql_native_password',
-            use_unicode=True
-        )
-        self.curr = self.conn.cursor()
+    # def create_connection(self):
+    #     self.conn = mysql.connector.connect(
+    #         host = 'localhost',
+    #         user = 'root',
+    #         password = 'Tmddms0722@',
+    #         database = 'land_DB',
+    #         charset = 'utf8',
+    #         auth_plugin='mysql_native_password',
+    #         use_unicode=True
+    #     )
+    #     self.curr = self.conn.cursor()
     
-    def create_table(self):
-        self.curr.execute("CREATE TABLE dong_list (dong_Name text, apartment_Name text,type_List text,pyeongName text,year text, month text, date text,price text, montly text)")
+    # def create_table(self):
+    #     self.curr.execute("CREATE TABLE naver_land (dong_Name text, apartment_Name text,type_List text,pyeongName text,year text, month text, date text,price text, montly text)")
 
     def process_item(self, item, spider):
-        self.store_db(item)
+        # self.store_db(item)
         return item
 
-    def store_db(self, item):
+    # def store_db(self, item):
 
-        self.curr.execute("INSERT INTO dong_list VALUES (%s, %s,%s,%s,%s,%s,%s,%s,%s)",(
-            item['dong_Name'],
-            item['apartment_Name'],
-            item['type_List'],
-            item['pyeongName'],
-            item['year'],
-            item['month'],
-            item['date'],
-            item['price'],
-            item['monthly']
-        ))
-
-        self.curr.execute("SELECT * FROM dong_list")
+    #     self.curr.execute("INSERT INTO naver_land VALUES (%s, %s,%s,%s,%s,%s,%s,%s,%s)",(
+    #         item['dong_Name'],
+    #         item['apartment_Name'],
+    #         item['type_List'],
+    #         item['pyeongName'],
+    #         item['year'],
+    #         item['month'],
+    #         item['date'],
+    #         item['price'],
+    #         item['monthly']
+    #     ))
         
-        self.conn.commit()
+    #     self.conn.commit()
--- scrapy_code/naver_land/naver_land/spiders/spider.py
+++ scrapy_code/naver_land/naver_land/spiders/spider.py
@@ -1,7 +1,9 @@
 from urllib import response
+from urllib import request
 from urllib.request import Request
 import scrapy
 import requests
+from scrapy import Request
 from naver_land.items import NaverLandItem
 from ast import literal_eval
 
@@ -19,24 +21,27 @@ headers = {
                 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
         }
 
-seoul = 'https://new.land.naver.com/api/regions/list?cortarNo=1100000000'  # 1100000000 서울시 
-seoul_List = literal_eval(requests.get(seoul,headers=headers).text)  # 서울시 APT를 text로 변환 시켜 dict로 저장
+MAIN_URL = 'https://new.land.naver.com/api/' #반복되는 MAIN URL
 
-gangnum = 'https://new.land.naver.com/api/regions/list?cortarNo={}' 
+REGION_FIRST_KEY = '1100000000' #서울시
 
-# 읍 / 면 /동
-regionKey = 'https://new.land.naver.com/api/regions/complexes?cortarNo={}' 
+REGION_SECOND_KEY = '1168000000' #강남구
 
-#아파트
-apart = 'https://new.land.naver.com/api/complexes/overview/{}?complexNo={}'
+REGION_FIRST = f'https://new.land.naver.com/api/regions/list?cortarNo={REGION_FIRST_KEY}' #시군구 URL
 
-detail = 'https://new.land.naver.com/api/complexes/{}/prices/real?complexNo={}&tradeType={}&year=5&priceChartChange=true&areaNo={}&type=table'
+REGION_FIRST_LIST = literal_eval(requests.get(REGION_FIRST, headers=headers).text)
+
+REGION_SECOND = 'https://new.land.naver.com/api/regions/list?cortarNo={}' #읍면동 URL
+
+DONG_APART = 'https://new.land.naver.com/api/regions/complexes?cortarNo={}' #동들의 아파트
+
+APART = 'https://new.land.naver.com/api/complexes/overview/{}?complexNo={}'
+
+DETAIL = 'https://new.land.naver.com/api/complexes/{}/prices/real?complexNo={}&tradeType={}&year=5&priceChartChange=true&areaNo={}&type=table'
 
 type = 'A1,B1,B2' #매매,전세,월세 주택유형ID 매매 3번째
 
 type_split = type.split(',')
-
-
 
 class SpiderSpider(scrapy.Spider):
     
@@ -45,38 +50,36 @@ class SpiderSpider(scrapy.Spider):
     
     def start_requests(self):
             
-            for i in seoul_List['regionList']:  #사울시 
+            for i in REGION_FIRST_LIST['regionList']:  
                 
-                if i['cortarNo'] == '1168000000' : #시/군/구는 강남구로 한정시킨다.
+                if i['cortarNo'] == REGION_SECOND_KEY : 
                     
                     cortarNo = i['cortarNo']
-                    gangnum_url = gangnum.format(cortarNo) #읍 면 동이 나올 url (개포동,청담동)   #https://new.land.naver.com/api/regions/list?cortarNo=1168011400
+                    REGION_SCOND_URL = REGION_SECOND.format(cortarNo) #읍 면 동이 나올 url (개포동,청담동) #https://new.land.naver.com/api/regions/list?cortarNo=1168011400
                     
-                    yield scrapy.Request(gangnum_url, self.parse, dont_filter=True)
+                    yield scrapy.Request(REGION_SCOND_URL, self.parse, dont_filter=True)
 
 
     def parse(self,response):
         items = []
-        gangnum_List = literal_eval(response.text)
+        REGION_SECOND_URL = literal_eval(response.text)
 
-        for i in gangnum_List['regionList']:
-            
-            # dong_Num = i['cortarNo']
-            
-            if i['cortarNo'] == '1168011400': #개포로 한정
+        print(response)
+
+        for i in REGION_SECOND_URL['regionList']:
                 dong_Num = i['cortarNo']
                 dong_Name = i['cortarName']
-                dong_url = regionKey.format(dong_Num) # 동 들이 나오는 url
-                dong_List = literal_eval(requests.get(dong_url,headers=headers).text)
+                DONG_APART_URL = DONG_APART.format(dong_Num) # 동 들이 나오는 url
+                dong_List = literal_eval(requests.get(DONG_APART_URL,headers=headers).text)
 
                 for i in dong_List['complexList']: #https://new.land.naver.com/api/complexes/overview/119219?complexNo=119219
                 
                     if i['realEstateTypeName'] == '아파트': #유형 형태에서 아파트만 추출
 
                             apartment_Num = i['complexNo'] #아파트 번호
-                            apartment_Name = i['complexName']
+                            apartment_Name = i['complexName'] #아파트 명
 
-                            apartment_url = apart.format(apartment_Num,apartment_Num)
+                            apartment_url = APART.format(apartment_Num,apartment_Num)
                             apartment_List = literal_eval(requests.get(apartment_url,headers=headers).text)
                             
                             for i in apartment_List['pyeongs']:
@@ -87,7 +90,7 @@ class SpiderSpider(scrapy.Spider):
                                 for i in type_split:
 
                                     type_List = i # 매매 전세 월세                
-                                    detail_url = detail.format(apartment_Num,apartment_Num,i,pyeongNo) #면적당 url뽑은거                                 
+                                    detail_url = DETAIL.format(apartment_Num,apartment_Num,i,pyeongNo) #면적당 url뽑은거                                 
                                     detail_List = literal_eval(requests.get(detail_url,headers=headers).text)
 
                                     for i in detail_List['realPriceOnMonthList']:
@@ -105,8 +108,6 @@ class SpiderSpider(scrapy.Spider):
                                             if type_List == 'A1': #매매
 
                                                 price = i['dealPrice']
-                                            # print(dong_Name,apartment_Num,apartment_Name,'매매 = ','타입 : ',detail_List['areaNo'],' 면적 : ',pyeongName, date,' 층수 : ',floor,' 가격 : ',price)
-                                                # item['매매'] = dong_Name,dong_Num,apartment_Name,type_List,date,floor,price
 
                                                 item['dong_Name'] = dong_Name
                                                 item['dong_Num'] = dong_Num
@@ -130,8 +131,7 @@ class SpiderSpider(scrapy.Spider):
 
                                                 price = i['leasePrice']
                                                 print(dong_Name,apartment_Num,apartment_Name,'전세 = ','타입 : ',detail_List['areaNo'],' 면적 : ',pyeongName, date,' 층수 : ',floor,' 가격 : ',price)
-                                                # item['전세'] = dong_Name,dong_Num,apartment_Name,apartment_Num,type_List,date,floor,price
-
+                                            
                                                 item['dong_Name'] = dong_Name
                                                 item['dong_Num'] = dong_Num
                                                 
@@ -152,14 +152,12 @@ class SpiderSpider(scrapy.Spider):
 
                                             else : #월세
 
-                                                # price = i['leasePrice'],i['rentPrice']
                                                 deposit = i['leasePrice']
                                                 monthly = i['rentPrice']
 
                                                 price = deposit,monthly
                     
                                                 print(dong_Name,apartment_Num,apartment_Name,'월세 = ','타입 : ',detail_List['areaNo'],' 면적 : ',pyeongName, date,' 층수 : ',floor,price)
-                                                # item['월세'] = dong_Name,dong_Num,apartment_Name,apartment_Num,type_List,date,floor,deposit,monthly
 
                                                 item['dong_Name'] = dong_Name
                                                 item['dong_Num'] = dong_Num
@@ -176,7 +174,6 @@ class SpiderSpider(scrapy.Spider):
                                                 item['floor'] = floor
                                                 item['price'] = deposit
                                                 item['monthly'] = monthly
-                                                # item['monthly'] = monthly
 
                                                 items.append(item)
                                         
